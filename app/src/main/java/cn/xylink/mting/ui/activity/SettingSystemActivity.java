7c6fe63b6fcaede810e9c0f6edbed8384ccc63c2
wangjingna <wangjingna@xylink.cn>
2020-01-15 14:54:08 +0800

    修改--将退出按钮移动到系统设置中 逻辑不变

--- app/src/main/java/cn/xylink/mting/ui/activity/SettingSystemActivity.java
+++ app/src/main/java/cn/xylink/mting/ui/activity/SettingSystemActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
+import com.tendcloud.tenddata.TCAgent;
+
 import butterknife.BindView;
 import butterknife.OnClick;
 import cn.xylink.mting.R;
+import cn.xylink.mting.ui.dialog.BottomAccountLogoutDialog;
+import cn.xylink.mting.utils.ContentManager;
 
 /**
  * @author wjn
@@ -36,7 +40,7 @@ public class SettingSystemActivity extends BasePresenterActivity {
         tvTitle.setText("系统设置");
     }
 
-    @OnClick({R.id.btn_left, R.id.ll_voice, R.id.ll_time, R.id.ll_font, R.id.ll_add})
+    @OnClick({R.id.btn_left, R.id.ll_voice, R.id.ll_time, R.id.ll_font, R.id.ll_add, R.id.btn_out})
     void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_left:
@@ -54,10 +58,41 @@ public class SettingSystemActivity extends BasePresenterActivity {
             case R.id.ll_add:
 
                 break;
+            case R.id.btn_out:
+                if (ContentManager.getInstance().getVisitor().equals("0")) {//表示是游客登录
+                    SettingSystemActivity.this.sendBroadcast(new Intent("action2exit"));
+                } else {
+                    showOutAccountDialog();
+                }
+                break;
             default:
 
                 break;
         }
+    }
+
+    private void showOutAccountDialog() {
+        BottomAccountLogoutDialog dialog = new BottomAccountLogoutDialog(this);
+        dialog.onClickListener(new BottomAccountLogoutDialog.OnBottomSelectDialogListener() {
+            @Override
+            public void onFirstClick() {
+                sendBroadcast(new Intent("action2exit"));
+            }
+
+            @Override
+            public void onSecondClick() {
+                TCAgent.onEvent(SettingSystemActivity.this, "account_exit");
+                ContentManager.getInstance().setUserInfo(null);
+                ContentManager.getInstance().setLoginToken("");
+                ContentManager.getInstance().setVisitor("0");//设置成游客
+                Intent intents = new Intent(SettingSystemActivity.this, LoginActivity.class);
+                intents.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intents.putExtra(LoginActivity.LOGIN_ACTIVITY, "outAccount");
+                startActivity(intents);
+                SettingSystemActivity.this.finish();
+            }
+        });
+        dialog.show();
     }
 
 }
